9c985fcc25d4bc41ceeec42efc2d1945357b9ab0
twoone-3 <62778573+twoone-3@users.noreply.github.com>
2021-02-10 19:41:47 +0800

    Tag

---
+++ NBT.hpp
@@ -0,0 +1,90 @@
+#pragma once
+#include "pch.h"
+#include "BDS.hpp"
+using namespace std;
+enum TagType {
+	End, Byte, Short, Int, Int64, Float,
+	Double, ByteArray, String, List, Compound,
+};
+using cm = map<string, struct CompoundTagVariant>;
+struct Tag {
+	VA vftable;
+	VA value[3];
+
+	cm& asCom() {
+		return *(cm*)(value);
+	}
+	Tag() {
+		SYMCALL("??0CompoundTag@@QEAA@XZ", this);
+	}
+	~Tag() {
+	}
+	void put(const string& key, const Tag* value) {
+		return SYMCALL("?put@CompoundTag@@QEAAAEAVTag@@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@$$QEAV2@@Z",
+			this, key, value);
+	}
+	void putByte(const string& key, const unsigned char value) {
+		return SYMCALL("?putByte@CompoundTag@@QEAAAEAEV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@E@Z",
+			this, key, value);
+	}
+	void putShort(const string& key, const short value) {
+		return SYMCALL("?putShort@CompoundTag@@QEAAAEAFV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@F@Z",
+			this, key, value);
+	}
+	void putString(const string& key, const string& value) {
+		return SYMCALL("?putString@CompoundTag@@QEAAAEAV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@V23@0@Z",
+			this, key, value);
+	}
+	void putInt(const string& key, const int& value) {
+		return SYMCALL("?putInt@CompoundTag@@QEAAAEAHV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@H@Z",
+			this, key, value);
+	}
+	void putInt64(const string& key, const int64_t& value) {
+		return SYMCALL("?putInt64@CompoundTag@@QEAAAEA_JV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@_J@Z",
+			this, key, value);
+	}
+	void putFloat(const string& key, const float& value) {
+		return SYMCALL("?putFloat@CompoundTag@@QEAAAEAMV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@M@Z",
+			this, key, value);
+	}
+	void putBoolean(const string& key, const bool& value) {
+		return SYMCALL("?putBoolean@CompoundTag@@QEAAXV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@_N@Z",
+			this, key, value);
+	}
+	void putCompound(const string& key, const Tag* value) {
+		//return SYMCALL("?putCompound@CompoundTag@@QEAAAEAV1@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@V1@@Z",
+		return SYMCALL("?putCompound@CompoundTag@@QEAAPEAV1@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@V?$unique_ptr@VCompoundTag@@U?$default_delete@VCompoundTag@@@std@@@3@@Z",
+			this, key, &value);
+	}
+
+	void add(Tag* t) {
+		SYMCALL("?add@ListTag@@QEAAXV?$unique_ptr@VTag@@U?$default_delete@VTag@@@std@@@std@@@Z",
+			this, &t);
+	}
+	void deCompound() {
+		SYMCALL("??1CompoundTag@@UEAA@XZ", this);
+	}
+	void deList() {
+		SYMCALL("??1ListTag@@UEAA@XZ", this);
+	}
+};
+struct CompoundTagVariant {
+	int type() {
+		return *((char*)this + 40);
+	}
+	auto& asByte() { return *(unsigned char*)((VA)this + 8); }
+	auto& asShort() { return *(short*)((VA)this + 8); }
+	auto& asInt() { return *(int*)((VA)this + 8); }
+	auto& asInt64() { return *(long long*)((VA)this + 8); }
+	auto& asFloat() { return *(float*)((VA)this + 8); }
+	auto& asDouble() { return *(double*)((VA)this + 8); }
+	auto& asString() { return *(string*)((VA)this + 8); }
+	auto asListTag() { return (Tag*)this; }
+	auto& asList() { return *(vector<Tag*>*)((VA)this + 8); }
+	auto& asCompound() { return *(map<string, CompoundTagVariant>*)((VA)this + 8); }
+};
+Tag* newTag(TagType t);
+Json::Value ListtoJson(Tag* t);
+Json::Value toJson(Tag* t);
+Tag* toTag(const Json::Value& j);
+Tag* ArraytoTag(const Json::Value& j);
