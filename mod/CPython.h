660f0af51aa6bed59e36b7c4ee2b90fc9d4e2123
WillowSauceR <WillowSauceR@outlook.com>
2022-01-26 12:40:13 +0800

    [From JFishing] fix code error

--- mod/CPython.h
+++ mod/CPython.h
@@ -8,7 +8,7 @@
 #define Py_PARSE_WITH_KERWORDS(format,...) if (!PyArg_ParseTupleAndKeywords(args, kwds, format ":" __FUNCTION__, const_cast<char**>(kwlist), __VA_ARGS__))return nullptr
 
 #define Py_RETURN_ERROR(str) return PyErr_SetString(PyExc_Exception, str), nullptr
-#define Py_PRINT_REFCOUNT(obj) cout << "ÒýÓÃ¼ÆÊý£º" << obj->ob_refcnt << endl
+#define Py_PRINT_REFCOUNT(obj) cout << "å¼•ç”¨è®¡æ•°ï¼š" << obj->ob_refcnt << endl
 //#define Py_BEGIN_CALL\
 //	int _has_gil = PyGILState_Check();\
 //	PyGILState_STATE _gil_state = PyGILState_LOCKED;\
@@ -20,12 +20,12 @@
 //	Py_END_ALLOW_THREADS;\
 //	if (!_has_gil)PyGILState_Release(_gil_state)
 
-//×Ö·û´®×ªUnicode
+//å­—ç¬¦ä¸²è½¬Unicode
 inline PyObject* ToPyStr(std::string_view str) {
 	return PyUnicode_InternFromString(str.data());
 	//return PyUnicode_FromStringAndSize(str.data(), str.length());
 }
-//´òÓ¡´íÎóÐÅÏ¢
+//æ‰“å°é”™è¯¯ä¿¡æ¯
 inline void PrintPythonError() {
 	if (PyErr_Occurred()) {
 		PyErr_Print();
