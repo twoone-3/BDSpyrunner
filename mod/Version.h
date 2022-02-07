277fa138ce06f3ea8551caec8b099859bb0d4c6e
twoone3 <3197653242@qq.com>
2022-02-08 07:13:38 +0800

    1.9.9更新，修复测试代码未删除

--- mod/Version.h
+++ mod/Version.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
 constexpr unsigned    PYR_VERSION_MAJOR = 1;
 constexpr unsigned    PYR_VERSION_MINOR = 9;
-constexpr unsigned    PYR_VERSION_MICRO = 8;
-constexpr const char* PYR_VERSION = "v1.9.8";
+constexpr unsigned    PYR_VERSION_MICRO = 9;
+constexpr const char* PYR_VERSION = "v1.9.9";
