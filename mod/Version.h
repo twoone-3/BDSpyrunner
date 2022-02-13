f089a7ea32f56ff34e68b3ce4144280fef2255ca
WillowSauceR <WillowSauceR@outlook.com>
2022-02-14 04:15:40 +0800

    update version

--- mod/Version.h
+++ mod/Version.h
@@ -2,4 +2,4 @@
 constexpr unsigned PYR_MAJOR_VERSION = 1;
 constexpr unsigned PYR_MINOR_VERSION = 8;
 constexpr unsigned PYR_MICRO_VERSION = 7;
-constexpr const char* PYR_VERSION = "v1.8.7";
+constexpr const char* PYR_VERSION = "v1.8.8b";
