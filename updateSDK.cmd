2f2fd19f7c17267ddcb7c1e6c47b8a47cdd09985
twoone3 <3197653242@qq.com>
2022-07-02 23:11:49 +0800

    update cmd

--- updateSDK.cmd
+++ updateSDK.cmd
@@ -2,7 +2,7 @@ git submodule deinit --all -f
 git rm --cached SDK
 rmdir SDK /S /Q
 rmdir ".git/modules" /S /Q
-git submodule add https://github.com/LiteLDev/LiteLoaderSDK SDK
+git submodule add --depth=1 https://github.com/LiteLDev/LiteLoaderSDK SDK
 cd SDK\Tools
 LibraryBuilder.exe -o ..\Lib "C:\Users\31976\Desktop\BDS"
 pause
